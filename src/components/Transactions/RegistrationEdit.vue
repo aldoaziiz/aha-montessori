bfb7304b3137496c6a3c90b07cc6ac7c7e30366c
aldoaziiz <aldoaziiz@gmail.com>
2026-08-31 16:21:26 +0800

    ubah learning period pendaftaran menjadi 1 - 12

--- src/components/Transactions/RegistrationEdit.vue
+++ src/components/Transactions/RegistrationEdit.vue
@@ -142,8 +142,18 @@
                 v-model="form.program_duration_months"
                 label="Learning Period"
                 :items="[
-                  { title: '6 Months', value: 6 },
-                  { title: '12 Months', value: 12 },
+                  { title: '1', value: 1 },
+                  { title: '2', value: 2 },
+                  { title: '3', value: 3 },
+                  { title: '4', value: 4 },
+                  { title: '5', value: 5 },
+                  { title: '6', value: 6 },
+                  { title: '7', value: 7 },
+                  { title: '8', value: 8 },
+                  { title: '9', value: 9 },
+                  { title: '10', value: 10 },
+                  { title: '11', value: 11 },
+                  { title: '12', value: 12 },
                 ]"
                 item-title="title"
                 item-value="value"
